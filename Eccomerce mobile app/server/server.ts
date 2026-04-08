305713c518fff72d52fe9671ebc03457e594d623
Weruh <169081731+Weruh@users.noreply.github.com>
2026-04-08 09:32:54 +0300

    clerk intergrated in backend server

--- Eccomerce mobile app/server/server.ts
+++ Eccomerce mobile app/server/server.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import type { Request, Response, } from 'express';
 import cors from "cors";
 import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -10,11 +11,15 @@ const app = express();
 await connectDB();
 
 
+
+
 // Middleware
 app.use(cors())
 app.use(express.json());
+app.use(clerkMiddleware())
 
 const port = process.env.PORT || 3000;
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
